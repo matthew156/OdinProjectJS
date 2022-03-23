d0b988affedfb137b5b6d32950f52b2a12386f74
Matt <matthewastro@gmail.com>
2022-03-22 23:07:20 -0400

    Finished the Project with full reset capabilities

--- Fundamentals/etch-a-sketch/script.js
+++ Fundamentals/etch-a-sketch/script.js
@@ -1,20 +1,48 @@
 const grid = document.getElementById('grid')
+const reset = document.getElementById('reset')
+
+
+
+// RESET BUTTON
+reset.addEventListener('click', function (){
+   while(grid.firstChild){
+       grid.removeChild(grid.firstChild);
+   }
+   i = 0
+   return Start(parseInt(prompt("How many Squares (Max: 100)")))
+})
+
 
 let i = 0;
 let j = 0;
+// Creates the sketch pad
+function Start(squares=100){
+
 while(i < 16)
 {
-    let columns = document.createElement("div");
+    columns = document.createElement("div");
     columns.classList.add('col');
     i++;
     grid.appendChild(columns)
-    while(j < 36)
+    while(j < squares)
     {
     let gray = document.createElement('div')
     gray.classList.add('gray')
+    gray.addEventListener("mouseover", 
+    function (){
+        gray.classList.remove()
+        gray.classList.add("black")
+    }
+    )
     j++;
     columns.appendChild(gray)
     }
-    j = 0;
+    j=0;
 }
+
+
+}
+
+Start()
+
 
